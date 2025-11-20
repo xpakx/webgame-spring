e8e1f735d83f5162516e0a6abcc3e3e187930a9e
xpakx <55586470+xpakx@users.noreply.github.com>
2025-11-20 01:19:47 +0100

    feat: overlay pixi ui and three.js canvases

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,12 +1,17 @@
 import { gsap } from 'gsap';
 import { Application, Assets, Sprite, TextureSource } from 'pixi.js';
+import {
+	Scene, PerspectiveCamera, WebGLRenderer, 
+	BoxGeometry, MeshBasicMaterial, Mesh
+} from "three";
 
 (async () => {
 	const app = new Application();
 	await app.init({
 		width: 800,
 		height: 600,
-		background: '#1099bb'
+		background: '#1099bb',
+		backgroundAlpha: 0,
 	});
 	document.body.appendChild(app.canvas);
 
@@ -33,5 +38,46 @@ import { Application, Assets, Sprite, TextureSource } from 'pixi.js';
 	});
 
 	app.stage.addChild(logo);
+
+
+	const threeCanvas = document.createElement("canvas");
+	threeCanvas.width = 800;
+	threeCanvas.height = 600;
+	document.body.appendChild(threeCanvas);
+	threeCanvas.style.position = "absolute";
+	threeCanvas.style.top = "0";
+	threeCanvas.style.left = "0";
+	threeCanvas.style.zIndex = "-1";
+
+	const scene = new Scene();
+	const camera = new PerspectiveCamera(
+		75,
+		800 / 600,
+		0.1,
+		1000
+	);
+	camera.position.z = 5;
+
+	const renderer = new WebGLRenderer({ canvas: threeCanvas, alpha: true });
+	renderer.setSize(800, 600);
+
+	const geometry = new BoxGeometry();
+	const material = new MeshBasicMaterial({ color: 0x00ff00 });
+	const cube = new Mesh(geometry, material);
+	scene.add(cube);
+
+	gsap.to(cube.rotation, {
+		x: Math.PI * 2,
+		y: Math.PI * 2,
+		duration: 5,
+		repeat: -1,
+		ease: "none",
+	});
+
+	function animate() {
+		requestAnimationFrame(animate);
+		renderer.render(scene, camera);
+	}
+	animate();
 })();
 
