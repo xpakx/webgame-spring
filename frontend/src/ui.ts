82304625425a8316cfdfcf141b40ef6f2fe95b21
xpakx <55586470+xpakx@users.noreply.github.com>
2025-11-22 23:04:44 +0100

    chore: remove pixi bunny example

--- frontend/src/ui.ts
+++ frontend/src/ui.ts
@@ -36,27 +36,6 @@ export class UIManager {
 
 	async loadAssets() {
 		TextureSource.defaultOptions.scaleMode = 'nearest';
-		const texture = await Assets.load('https://pixijs.com/assets/bunny.png');
-
-			this.logo = new Sprite({
-			texture,
-			anchor: 0.5,
-			x: this.width/2,
-			y: this.height/2,
-		});
-		this.logo.width = 100;
-		this.logo.scale.y = this.logo.scale.x;
-		this.logo.eventMode = 'static';
-
-		const xTo = gsap.quickTo(this.logo, 'x', { duration: 0.6, ease: 'power3' });
-		const yTo = gsap.quickTo(this.logo, 'y', { duration: 0.6, ease: 'power3' });
-		this.logo.on('globalpointermove', (event) => {
-			xTo(event.global.x);
-			yTo(event.global.y);
-		});
-
-		this.stage.addChild(this.logo);
-
 		await this.createMainHud();
 	}
 
